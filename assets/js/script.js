c826c9cf912688b0dd57773ae8e1e6ea139c1fa6
Becca Doan <rebecca.doan@cirrus.com>
2022-06-29 15:08:37 -0500

    finished html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log("testing testing 123!");
+// grabs coordinates of city
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
+// grab weather data from lat and long
+// http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}
+// units = imperial
