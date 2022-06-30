9d9f44996e76afc5a26e1d0b281234e9202fb780
Becca Doan <rebecca.doan@cirrus.com>
2022-06-29 21:21:43 -0500

    finished gathering weather data and printing it to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,11 @@ var currentTempEl = document.querySelector("#temp");
 var currentHumidEl = document.querySelector("#humidity");
 var currentWindEl = document.querySelector("#wind");
 var currentUVEl = document.querySelector("#UV");
-var fiveDayEl = document.querySelector("#day-container");
-
-
-// cityNameEl.textContent = moment().format("L");
+var fiveDayEl = document.querySelector(".day-container");
+var formEl = document.querySelector("#submit-city");
+var citySearchEl = document.querySelector("#city-search")
 // cityNameEl.textContent += moment().add(2, 'days').format("L")
+// cityNameEl.textContent = "fu"
 
 var getLatLong = function(city){
     var url = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid=' + apiKey
@@ -59,8 +59,36 @@ var populateCurrentData = function(weather) {
 
 var populateFutureData = function(weather) {
     var dailyArray = weather.slice(1,6);
-    console.log(dailyArray);
+    fiveDayEl.innerHTML = ""
+    for (var i = 0; i < 5; i++){
+        // console.log(dailyArray[i]);
+        var dailyForecastDiv = document.createElement('div');
+        dailyForecastDiv.classList.add("one-day-forecast");
+        var dateEl = document.createElement('h4');
+        dateEl.textContent = moment().add(i+1, 'days').format("L");
+        var icon = dailyArray[i].weather[0].icon;
+        var source = 'http://openweathermap.org/img/wn/'+icon+'@2x.png';
+        var iconEl = document.createElement('img');
+        iconEl.classList.add("icon-today")
+        iconEl.setAttribute('src', source);
+        var tempEl = document.createElement('p');
+        console.log(dailyArray[i]);
+        tempEl.innerHTML = "Temp: " + dailyArray[i].temp.day + "&deg;F";
+        var windEl = document.createElement('p');
+        windEl.textContent = "Wind: " + dailyArray[i].wind_speed + "MPH";
+        var humidEl = document.createElement('p');
+        humidEl.textContent = "Humidity: " + dailyArray[i].humidity + "%";
+
+
+        dailyForecastDiv.append(dateEl, iconEl, tempEl, humidEl, windEl);
+        fiveDayEl.append(dailyForecastDiv);
+    }
 }
 
-getLatLong("austin");
+formEl.addEventListener('submit', function(event){
+    event.preventDefault();
+    var name = citySearchEl.value.trim();
+    getLatLong(name);
+})
+
 
