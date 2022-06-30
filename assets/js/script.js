8229546d37f0976beaa898ff709d37989efd9575
Becca Doan <rebecca.doan@cirrus.com>
2022-06-29 20:20:47 -0500

    using the API to fetch data about a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,66 @@
 // grabs coordinates of city
-// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+var apiKey = 'c815eea855df2241ab24e06d69f9fa74';
 
-// grab weather data from lat and long
-// http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}
-// units = imperial
+var currentWeatherIcon = document.querySelector('#icon-today');
+var cityNameEl = document.querySelector("#name-date");
+var currentTempEl = document.querySelector("#temp");
+var currentHumidEl = document.querySelector("#humidity");
+var currentWindEl = document.querySelector("#wind");
+var currentUVEl = document.querySelector("#UV");
+var fiveDayEl = document.querySelector("#day-container");
+
+
+// cityNameEl.textContent = moment().format("L");
+// cityNameEl.textContent += moment().add(2, 'days').format("L")
+
+var getLatLong = function(city){
+    var url = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid=' + apiKey
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR");
+            }
+            return response.json();
+        }).then(data => {
+            var lat = data[0].lat.toString();
+            var lon = data[0].lon.toString();
+            var name = data[0].name;
+            cityNameEl.innerHTML = name + " (" + moment().format("L") + ") <span><img id='c-icon' class='w-icon'></span>";
+            getCityData(lat,lon);
+        })
+    }
+
+var getCityData = function(lat, lon) {
+    var url = 'http://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial&appid=' + apiKey;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw Error("ERROR");
+            }
+            return response.json();
+        }).then(data => {
+            var todayIconEl = document.querySelector("#c-icon");
+            var icon = data.current.weather[0].icon;
+            var source = 'http://openweathermap.org/img/wn/'+icon+'@2x.png';
+            todayIconEl.setAttribute('src', source);
+            populateCurrentData(data.current);
+            populateFutureData(data.daily);
+        })
+}
+
+var populateCurrentData = function(weather) {
+    currentTempEl.textContent = weather.temp;
+    currentWindEl.textContent = weather.wind_speed;
+    currentHumidEl.textContent = weather.humidity;
+    currentUVEl.textContent = weather.uvi;
+}
+
+var populateFutureData = function(weather) {
+    var dailyArray = weather.slice(1,6);
+    console.log(dailyArray);
+}
+
+getLatLong("austin");
+
